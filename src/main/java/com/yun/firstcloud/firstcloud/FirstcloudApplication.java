ba0c8e456601d92a1d0c9e089162a673d28efaca
muhammad.yunus <muhammad.yunus@rsystems.com>
2018-04-25 18:37:05 +0530

    Initial Checkin

---
+++ src/main/java/com/yun/firstcloud/firstcloud/FirstcloudApplication.java
@@ -0,0 +1,13 @@
+package com.yun.firstcloud.firstcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstcloudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirstcloudApplication.class, args);
+		System.out.println("Hello First Cloud");
+	}
+}
